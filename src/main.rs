12934718135b88f27de43f48691ca2b8498a2599
ellesmere <ellesmere6600@gmail.com>
2026-02-24 02:40:46 +0300

    feat: add basic structure for parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::env;
+
 fn main() {
-    println!("todo!");
+    // Получаем аргументы в виде итератора и преобразуем его в коллецию Vec
+    // Пропускаем первый аргумент, т.к. он является путём к исполняемому файлу
+    let args: Vec<String> = env::args().skip(1).collect();
+
+        // Проверка на количество аргументов
+    if args.len() < 1 {
+        println!("Not enough arguments!");
+    }
+
+    // Матчинг команд
+    match args[0].as_str() {
+        // Добавление задачи
+        "add" => {
+            println!("You called add");
+        }
+        // Удаление задачи
+        "del" => {
+            println!("You called del");
+        }
+        // Просмотреть задачи
+        "show" => {
+            println!("You called show");
+        }
+        // Отметить задачу
+        "mark" => {
+            println!("You called mark");
+        }
+        // Сохранить задачи в файл
+        "save" => {
+            println!("You called save");
+        }
+        // Неизвестная задача
+        _ => {
+            println!("Unknown command");
+        }
+    }
 }
